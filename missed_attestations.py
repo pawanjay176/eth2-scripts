8588c3424ac703dc0f81b4d11c22f2446bd57a55
pawan <pawandhananjay@gmail.com>
2020-09-08 13:12:23 +0530

    Minor refactoring

--- missed_attestations.py
+++ missed_attestations.py
@@ -4,7 +4,7 @@ import time
 VALIDATORS = [6686, 6687, 6690, 6717, 6718, 6719, 6720, 6721]
 
 # Time intervals for checking missed attestations in seconds
-interval = 300
+INTERVAL = 300
 
 BASE_URL = "https://beaconcha.in/api/v1/validator/"
 
@@ -13,12 +13,12 @@ def check():
         for validator in VALIDATORS:
             url = BASE_URL + str(validator) + "/attestations"
             data = requests.get(url).json()['data']
-            # Check only last 5 epochs
-            for i in range(1, 6):
+            # Check only last 3 epochs
+            for i in range(1, 3):
                 if data[i]['status'] == 0:
                     print("validator:", validator, " missed attestation: ", data[i]['attesterslot'])
                 if data[i]['attesterslot'] % 32 == 0 or data[i]['attesterslot'] % 32 == 1:
-                    print("validator: ", validator, "on epoch boundary: ", data[i]['attesterslot'])
-        print("Completed check, sleeping")
-        time.sleep(interval)
+                    print("validator: ", validator, "on epoch boundary: ", data[i]['epoch'])
+        print("Completed check, sleeping\n\n")
+        time.sleep(INTERVAL)
 check()
